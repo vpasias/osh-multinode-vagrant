708664156aa81ca53a80025a9079aa5bfa00ff56
Drew Walters <drewwalters96@gmail.com>
2019-07-25 13:18:13 +0000

    Deploy on n1 instead of host machine

--- 01-deploy-multinode-k8s.sh
+++ 01-deploy-multinode-k8s.sh
@@ -2,45 +2,24 @@
 
 set -xe
 
-START_DIR=$(pwd)
-
-sudo chown -R ubuntu: /opt
-rm -rf /opt/openstack-helm*
-git clone https://git.openstack.org/openstack/openstack-helm-infra.git /opt/openstack-helm-infra
-git clone https://git.openstack.org/openstack/openstack-helm.git /opt/openstack-helm
-
-cat > /opt/openstack-helm-infra/tools/gate/devel/multinode-inventory.yaml <<EOF
-all:
-  children:
-    primary:
-      hosts:
-        node_one:
-          ansible_port: 22
-          ansible_host: 192.168.50.4
-          ansible_user: ubuntu
-          ansible_ssh_private_key_file: /home/ubuntu/etc/.vagrant/machines/n1/virtualbox/private_key
-          ansible_ssh_extra_args: -o "LogLevel=FATAL" -o "Compression=yes" -o "DSAAuthentication=yes" -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null"
-    nodes:
-      hosts:
-        node_two:
-          ansible_port: 22
-          ansible_host: 192.168.50.5
-          ansible_user: ubuntu
-          ansible_ssh_private_key_file: /home/ubuntu/etc/.vagrant/machines/n2/virtualbox/private_key
-          ansible_ssh_extra_args: -o "LogLevel=FATAL" -o "Compression=yes" -o "DSAAuthentication=yes" -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null"
-        node_three:
-          ansible_port: 22
-          ansible_host: 192.168.50.6
-          ansible_user: ubuntu
-          ansible_ssh_private_key_file: /home/ubuntu/etc/.vagrant/machines/n3/virtualbox/private_key
-          ansible_ssh_extra_args: -o "LogLevel=FATAL" -o "Compression=yes" -o "DSAAuthentication=yes" -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null"
-EOF
+pushd $(pwd)
 
 cd etc
+
 vagrant up
 
-cd /opt/openstack-helm-infra
-make dev-deploy setup-host multinode
-make dev-deploy k8s multinode
+scp -i .vagrant/machines/n1/virtualbox/private_key .vagrant/machines/n1/virtualbox/private_key ubuntu@192.168.50.4:.ssh/n1
+scp -i .vagrant/machines/n1/virtualbox/private_key .vagrant/machines/n2/virtualbox/private_key ubuntu@192.168.50.4:.ssh/n2
+scp -i .vagrant/machines/n1/virtualbox/private_key .vagrant/machines/n3/virtualbox/private_key ubuntu@192.168.50.4:.ssh/n3
 
-cd "${START_DIR}"
+scp -i .vagrant/machines/n2/virtualbox/private_key .vagrant/machines/n1/virtualbox/private_key ubuntu@192.168.50.5:.ssh/n1
+scp -i .vagrant/machines/n2/virtualbox/private_key .vagrant/machines/n2/virtualbox/private_key ubuntu@192.168.50.5:.ssh/n2
+scp -i .vagrant/machines/n2/virtualbox/private_key .vagrant/machines/n3/virtualbox/private_key ubuntu@192.168.50.5:.ssh/n3
+
+scp -i .vagrant/machines/n3/virtualbox/private_key .vagrant/machines/n1/virtualbox/private_key ubuntu@192.168.50.6:.ssh/n1
+scp -i .vagrant/machines/n3/virtualbox/private_key .vagrant/machines/n2/virtualbox/private_key ubuntu@192.168.50.6:.ssh/n2
+scp -i .vagrant/machines/n3/virtualbox/private_key .vagrant/machines/n3/virtualbox/private_key ubuntu@192.168.50.6:.ssh/n3
+
+popd
+
+ssh -i etc/.vagrant/machines/n1/virtualbox/private_key ubuntu@192.168.50.4 "sudo apt-get update ; sudo apt-get install make ; cd /opt/openstack-helm-infra ; make dev-deploy setup-host multinode ; make dev-deploy k8s multinode"
