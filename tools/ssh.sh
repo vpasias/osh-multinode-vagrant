a08e9ecb7aa4bb3020c1b9c773dd3112ac2e9481
Drew Walters <drewwalters96@gmail.com>
2019-07-26 18:00:08 +0000

    Update SSH script
    The IP addresses in the SSH script are invalid. This commit updates them
    to match the Vagrantfile and refactors the SSh script.

--- tools/ssh.sh
+++ tools/ssh.sh
@@ -1,11 +1,21 @@
-if [[ $1 == "n1" ]]; then
-  ssh ubuntu@172.21.1.10 -i etc/.vagrant/machines/n1/virtualbox/private_key
+#!/bin/bash
+
+set -e
+
+declare -A \
+  NODES=( ["n1"]="172.24.1.10" ["n2"]="172.24.1.11" ["n3"]="172.24.1.12" )
+
+if [[ "$1" != "n1" && "$1" != "n2" && "$1" != "n3" ]]; then
+  echo "Error: invalid node. Valid nodes include n1, n2, n3"
+  echo "Example usage:"
+  echo "  ./tools/ssh.sh n1"
+  exit 1
 fi
 
-if [[ $1 == "n2" ]]; then
-  ssh ubuntu@172.21.1.11 -i etc/.vagrant/machines/n2/virtualbox/private_key
-fi
-
-if [[ $1 == "n3" ]]; then
-  ssh ubuntu@172.21.1.12 -i etc/.vagrant/machines/n3/virtualbox/private_key
-fi
+ssh -o "LogLevel=FATAL" \
+    -o "Compression=yes" \
+    -o "DSAAuthentication=yes" \
+    -o "IdentitiesOnly=yes" \
+    -o "StrictHostKeyChecking=no" \
+    -o "UserKnownHostsFile=/dev/null" \
+    -i "etc/.vagrant/machines/$1/virtualbox/private_key" ubuntu@"${NODES[$1]}"
