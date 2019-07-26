804a991434187852f62a18ef9809d6e460f26ae0
Drew Walters <drewwalters96@gmail.com>
2019-07-26 19:26:28 +0000

    Fix shell splitting error
    Splitting was not occurring client-side on SSH commands, causing
    failures. This commit updates the script to process splitting on the
    cient-side and updates the CI to ignore error SC2029.

--- tools/gate/shellcheck.sh
+++ tools/gate/shellcheck.sh
@@ -5,6 +5,4 @@ set -xe
 sudo apt-get update
 sudo apt-get install --no-install-recommends -y shellcheck
 
-shellcheck *.sh --shell=bash
-shellcheck etc/*.sh --shell=bash
-shellcheck tools/*.sh --shell=bash
+shellcheck -e SC2029 --shell=bash *.sh etc/*.sh tools/*.sh
